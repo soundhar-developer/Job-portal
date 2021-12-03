81d6525df94a021944279b547fab7f1de89e95bf
soundhar-developer <86889335+soundhar-developer@users.noreply.github.com>
2021-12-03 12:07:02 +0530

    Update jobseekerdashboard.blade.php
    Updated Changes

--- resources/views/dashboard/jobseekerdashboard.blade.php
+++ resources/views/dashboard/jobseekerdashboard.blade.php
@@ -222,7 +222,6 @@
                 <footer class="pt-4 my-md-5 pt-md-5 border-top">
                     <div class="row">
                       <div class="col-12 col-md">
-                        <!-- <img class="mb-2" src="../assets/brand/bootstrap-logo.svg" alt="" width="24" height="19"> -->
                         <small class="d-block mb-3 text-muted">&copy; 2021–2022</small>
                       </div>
                     </div>
